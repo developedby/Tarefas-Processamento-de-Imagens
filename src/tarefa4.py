1de385f190b3b1c65cf43b58c638edc94318be75
Nicolas Abril <nicolas.abril@gmail.com>
2019-09-29 22:10:51 -0300

    Termina tarefa4

--- src/tarefa4.py
+++ src/tarefa4.py
@@ -12,12 +12,13 @@ from pdi import unsharp, labelize, uint8_to_float
 
 # Parâmetros
 IMAGE_FOLDER = "../img/tarefa4"
-INPUT_IMAGE = "150.bmp"#"60.bmp"
-THRESHOLD = 0.7
-NUM_ITER_MEAN = 3
-BLOOM_WEIGHT = 0.12
-NUM_BLURS = 5
-INIT_SIGMA = 5
+INPUT_IMAGE = "60.bmp"
+
+UNSHARP_SIGMA_DIVIDER = 200
+UNSHARP_WEIGHT = 1
+THRESHOLD = 240
+AREA_CUTOFF = 0.2
+AREA_CORRECTION = [0.02, 0.01, 0.002]
 
 if __name__ == '__main__':
     # Abre a imagem
@@ -25,31 +26,41 @@ if __name__ == '__main__':
     if img is None:
         print("Erro ao abrir a imagem")
         exit()
-
     h, w, _ = img.shape
-
-    #f_img = uint8_to_float(img)
     gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-    #blur = cv2.GaussianBlur(gray, (0, 0), INIT_SIGMA)
-    #cv2.imwrite(f"{IMAGE_FOLDER}/blur.bmp", blur)
-    norma = np.zeros(gray.shape)
-    norma = cv2.normalize(gray,  norma, 150, 50, cv2.NORM_MINMAX)
-    cv2.imwrite(f"{IMAGE_FOLDER}/norma.bmp", norma)
-    sharp = unsharp(norma, 2*((w+h)//100)+1, 3)
-    out_img = (sharp*255).astype(np.uint8)
-    cv2.imwrite(f"{IMAGE_FOLDER}/sharp.bmp", sharp)
-    #thresh = cv2.adaptiveThreshold(sharp, 255,
-    #                               cv2.ADAPTIVE_THRESH_GAUSSIAN_C,
-    #                               cv2.THRESH_BINARY, 77, -1)
-    _, thresh = cv2.threshold(sharp, 200, 255, cv2.THRESH_BINARY)
-    cv2.imwrite(f"{IMAGE_FOLDER}/thresh.bmp", thresh)
+
+    norma = cv2.normalize(gray, None, 0, 255, cv2.NORM_MINMAX)
+    sharp = unsharp(norma,
+                    2*(max(w, h)//UNSHARP_SIGMA_DIVIDER)+1,
+                    UNSHARP_WEIGHT)
+    cv2.imwrite(f"{IMAGE_FOLDER}/1-sharp.bmp", sharp)
+
+    _, thresh = cv2.threshold(sharp, THRESHOLD, 255, cv2.THRESH_BINARY)
+    cv2.imwrite(f"{IMAGE_FOLDER}/2-thresh.bmp", thresh)
 
     kernel = cv2.getStructuringElement(cv2.MORPH_RECT, (3, 3))
-    open = cv2.morphologyEx(thresh, cv2.MORPH_OPEN, kernel)
-    cv2.imwrite(f"{IMAGE_FOLDER}/open.bmp", open)
-    eroded = cv2.erode(open, kernel, iterations=1)
-    cv2.imwrite(f"{IMAGE_FOLDER}/eroded.bmp", eroded)
+    open_ = cv2.morphologyEx(thresh, cv2.MORPH_OPEN, kernel)
+    cv2.imwrite(f"{IMAGE_FOLDER}/3-open.bmp", open_)
 
-    f_img = uint8_to_float(eroded)
+    f_img = uint8_to_float(open_)
     components = labelize(f_img, 1, 1, 1)
-    print(len(components))
+    
+    areas = np.array(list(map(len, components)))
+    median = np.median(areas)
+    rel_areas = areas / median
+
+    corrected_areas = rel_areas[rel_areas > AREA_CUTOFF]
+    poly_correction = np.zeros(corrected_areas.shape)
+    for order, weight in enumerate(AREA_CORRECTION):
+        order_n_correction = np.power(corrected_areas, order+1)*weight
+        poly_correction += order_n_correction
+    corrected_areas += poly_correction
+    corrected_areas = np.round(corrected_areas).astype(int)
+
+    num_rice = 0
+    for area in corrected_areas:
+        if area < 1:
+            num_rice += 1
+        else:
+            num_rice += int(round(area))
+    print(num_rice)
